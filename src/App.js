c2edce7d343634cbb5ebdb6861d30062ba276562
xaxi <francisco.perez@thebridgeschool.es>
2022-06-23 14:42:39 +0200

    conexión con el backend simple

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
 
 function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await fetch("http://localhost:3050/preguntas/");
+      const json = await res.json();
+      setData(json);
+    };
+    fetchData();
+  }, [setData]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ul>
+      {data.map((item) => (
+        <div>{item.pregunta}</div>
+      ))}
+    </ul>
   );
 }
 
